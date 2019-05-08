4ec9bd6f3c59f4c6390c9371c4eabefc2ea6d30c
Krosantos <tymko.tecca@gmail.com>
2019-05-08 18:55:03 -0400

    Non-functional buttons

--- src/components/DeckList/index.js
+++ src/components/DeckList/index.js
@@ -2,16 +2,25 @@ import React, { useMemo } from 'react';
 import useCards from '@hooks/useCards';
 import CardList from '@components/CardList';
 import { IS_IN_DECK } from '@constants';
-import { deckList } from './styles.scss';
+import { deckList, buttonContainer,importButton } from './styles.scss';
+
+const IMPORT = 'Import Deck';
+const EXPORT = 'Export Deck';
 
 const DeckList = () => {
 	const { cards, cardsByAttribute } = useCards();
 	const cardsInDeck = useMemo(() => cardsByAttribute(IS_IN_DECK), [cards]);
 
 	return (
-		<div className={deckList}>
-			<CardList cards={cardsInDeck} />
-		</div>
+		<>
+			<div className={deckList}>
+				<CardList cards={cardsInDeck} />
+			</div>
+			<div className={buttonContainer}>
+				<button className={importButton} type="button">{IMPORT}</button>
+				<button className={importButton} type="button">{EXPORT}</button>
+			</div>
+		</>
 	);
 };
 
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,23 +3,25 @@ import Slider from '@components/Slider';
 import AddCards from '@components/AddCards';
 import ContextMenu from '@components/ContextMenu';
 import DeckList from '@components/DeckList';
-import SaveButton from '@components/Debug/SaveButton';
+import useAutoSave from '@hooks/useAutoSave';
 import Header from './Header';
 import MainPanel from './MainPanel';
 
-const Layout = () => (
-	<>
-		<Header />
-		<Slider label="Deck" left>
-			<DeckList />
-		</Slider>
-		<Slider label="Search">
-			<AddCards />
-		</Slider>
-		<MainPanel />
-		<SaveButton />
-		<ContextMenu />
-	</>
-);
+const Layout = () => {
+	useAutoSave();
+	return (
+		<>
+			<Header />
+			<Slider label="Deck" left>
+				<DeckList />
+			</Slider>
+			<Slider label="Search">
+				<AddCards />
+			</Slider>
+			<MainPanel />
+			<ContextMenu />
+		</>
+	);
+};
 
 export default Layout;
