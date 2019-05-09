57ce6d3acdc4e0893cf488efd4593d8fae97aa2b
Tymko Tecca <tymko.tecca@gmail.com>
2019-05-08 23:34:48 -0400

    Cleaned up after myself

--- src/utils/getAverageCmc.js
+++ src/utils/getAverageCmc.js
@@ -1,15 +1,10 @@
-import includes from 'lodash/includes';
-import filter from 'lodash/filter';
 import values from 'lodash/values';
 
 const getAverageCmc = (cardObject) => {
 	const cards = values(cardObject);
 	const nonLand = cards.filter((card) => !card.type.includes('Land'));
-
 	const totalMana = cards.reduce((prev, curr) => (prev + curr.cmc), 0);
 
-	console.log(nonLand.length);
-	// cards.forEach((card) => console.log(card.type));
 	return totalMana / nonLand.length;
 };
 
