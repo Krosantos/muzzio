ae776e770b510c2f644907f6edd0e24049cf4908
Krosantos <tymko.tecca@gmail.com>
2019-05-11 00:31:14 -0400

    Style tweaks

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,11 @@
-import React, { useContext } from 'react';
-import { CardContext } from '@contexts/Card';
+import React from 'react';
 import Commander from '@components/Commander';
 import { header } from './styles.scss';
 
-const Header = () => {
-	const { cards } = useContext(CardContext);
-	const count = Object.keys(cards).length;
-
-	return (
-		<div className={header}>
-			<Commander />
-			{count}
-		</div>
-	);
-};
+const Header = () => (
+	<div className={header}>
+		<Commander />
+	</div>
+);
 
 export default Header;
