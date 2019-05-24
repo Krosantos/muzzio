fd763200c5cb9fbbc69cea444f1e655ecc4d6146
Krosantos <tymko.tecca@gmail.com>
2019-05-23 20:13:23 -0400

    Fix format loading bug

--- src/hooks/useOverwrite.js
+++ src/hooks/useOverwrite.js
@@ -2,14 +2,23 @@ import { useContext, useCallback } from 'react';
 import { CommanderContext } from '@contexts/Commander';
 import { CardContext } from '@contexts/Card';
 import { AttributesContext } from '@contexts/Attributes';
+import { OathbreakerContext } from '@contexts/Oathbreaker';
+import { FormatContext } from '@contexts/Format';
 import { BasicLandContext } from '@contexts/BasicLand';
-import { ALL_CARDS, OVERWRITE } from '@constants';
+import {
+	ALL_CARDS,
+	OVERWRITE,
+	COMMANDER,
+} from '@constants';
 
+// eslint-disable-next-line max-statements
 const useOverwrite = () => {
 	const { setAttributes } = useContext(AttributesContext);
 	const { setBasicLand } = useContext(BasicLandContext);
 	const { dispatch: cardsDispatch } = useContext(CardContext);
 	const { setCommanderData } = useContext(CommanderContext);
+	const { setOathbreakerData } = useContext(OathbreakerContext);
+	const { setFormat } = useContext(FormatContext);
 
 	const overwrite = useCallback((saveData = {}) => {
 		const {
@@ -17,8 +26,12 @@ const useOverwrite = () => {
 			basicLand = {},
 			cards = {},
 			commanderData = {},
+			format = COMMANDER,
+			oathbreakerData = {},
 		} = saveData;
 
+		setFormat(format);
+		setOathbreakerData(oathbreakerData);
 		setAttributes(attributes);
 		setBasicLand(basicLand);
 		cardsDispatch({ card: cards, type: OVERWRITE });
