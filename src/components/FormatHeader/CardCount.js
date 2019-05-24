a27f6b4b38503fd7d5509a97f498b30f90c79155
Krosantos <tymko.tecca@gmail.com>
2019-05-23 21:16:21 -0400

    Change card count to compensate for command zone

--- src/components/FormatHeader/CardCount.js
+++ src/components/FormatHeader/CardCount.js
@@ -1,7 +1,11 @@
+/* eslint-disable max-depth */
 import React, { useMemo } from 'react';
+import isEmpty from 'lodash/isEmpty';
 import useCards from '@hooks/useCards';
 import useBasicLands from '@hooks/useBasicLands';
 import useFormat from '@hooks/useFormat';
+import useCommander from '@hooks/useCommander';
+import useOathbreaker from '@hooks/useOathbreaker';
 import getAverageCmc from '@utils/getAverageCmc';
 import {
 	IS_IN_DECK,
@@ -13,22 +17,49 @@ import {
 import { cardCount } from './styles.scss';
 
 const formatCounts = {
-	[COMMANDER]: 99,
+	[COMMANDER]: 100,
 	[MODERN]: 60,
-	[OATHBREAKER]: 58,
+	[OATHBREAKER]: 60,
 	[STANDARD]: 60,
 };
 const CMC = 'CMC: ';
+
+const useCommandZoneCards = (format) => {
+	const { commander, partner } = useCommander();
+	const { oathbreaker, signatureSpell } = useOathbreaker();
+	// eslint-disable-next-line max-statements, complexity
+	const commandZoneCount = useMemo(() => {
+		let result = 0;
+
+		if (format === COMMANDER) {
+			if (!isEmpty(commander))
+				result += 1;
+			if (!isEmpty(partner))
+				result += 1;
+		}
+		if (format === OATHBREAKER) {
+			if (!isEmpty(oathbreaker))
+				result += 1;
+			if (!isEmpty(signatureSpell))
+				result += 1;
+		}
+		return result;
+	},
+	[format, commander, partner, oathbreaker, signatureSpell]);
+
+	return commandZoneCount;
+};
 
 // eslint-disable-next-line max-statements
 const CardCount = () => {
 	const { cardsByAttribute } = useCards();
 	const { totalCount: basicCount } = useBasicLands('');
-	const cardsInDeck = cardsByAttribute(IS_IN_DECK);
-	const count = cardsInDeck.length + basicCount;
-	const cmc = getAverageCmc(cardsInDeck).toPrecision(3);
 	const { format } = useFormat();
+	const commandZoneCount = useCommandZoneCards(format);
 	const OUT_OF_X = useMemo(() => `/${formatCounts[format]}`, [format]);
+	const count = useMemo(() => cardsByAttribute(IS_IN_DECK).length + basicCount + commandZoneCount,
+		[cardsByAttribute, basicCount, commandZoneCount]);
+	const cmc = getAverageCmc(cardsByAttribute(IS_IN_DECK)).toPrecision(3);
 
 	return (
 		<div className={cardCount}>
