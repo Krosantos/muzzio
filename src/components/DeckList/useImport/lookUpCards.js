947573d756bbb9344214843cf45388f42c6c0e70
Tymko Tecca <tymko.tecca@gmail.com>
2019-06-04 13:51:30 -0400

    Fix import to work with count

--- src/components/DeckList/useImport/lookUpCards.js
+++ src/components/DeckList/useImport/lookUpCards.js
@@ -1,33 +1,60 @@
+import get from 'lodash/get';
+import isEmpty from 'lodash/isEmpty';
 import getList from '@api/getList';
 
-const sanitizeLine = (line) => {
-	const noNumbers = line.replace(/\d /, '');
-	const noSet = noNumbers.replace(/\[.*\]/, '');
+const COUNT_REGEX = /\d /;
+const SET_REGEX = /\[.*\]/;
 
-	return noSet;
+const parseLine = (line) => {
+	const noNumbers = line.replace(COUNT_REGEX, '');
+	const rawName = noNumbers.replace(SET_REGEX, '');
+	const name = rawName.toLowerCase();
+	const countMatch = line.match(COUNT_REGEX);
+	const rawCount = get(countMatch, 0, 1);
+	const count = parseInt(rawCount, 10);
+
+	return { count, name };
+};
+
+const getCountMap = (lines) => {
+	const countMap = {};
+
+	lines.forEach((line) => {
+		const { count, name } = parseLine(line);
+
+		if (!isEmpty(name))
+			countMap[name] = count;
+	});
+	return countMap;
 };
 
 const splitIntoLines = (rawData) => rawData.split(/[\r\n]+/g);
 
-const convertToPostBody = (lines) => {
+const convertToPostBody = (countMap = {}) => {
 	const result = [];
+	const cards = Object.keys(countMap);
 
-	lines.forEach((line) => {
-		if (!line)
+	cards.forEach((card) => {
+		if (!card)
 			return;
-		result.push({ name: line });
+		result.push({ name: card });
 	});
 	return result;
 };
 
 const lookUpCards = async (rawData) => {
 	const rawLines = splitIntoLines(rawData);
-	const lines = rawLines.map(sanitizeLine);
-	const postBody = convertToPostBody(lines);
+	const countMap = getCountMap(rawLines);
+	const postBody = convertToPostBody(countMap);
 
 	const cards = await getList(postBody);
 
-	return cards.filter((card) => !card.type.includes('Basic Land'));
+	return cards.map((card) => {
+		const { name = '' } = card;
+		const count = countMap[name.toLowerCase()] || 1;
+
+		return { ...card, count };
+	});
 };
 
 export default lookUpCards;
