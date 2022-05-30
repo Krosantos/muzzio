c0718c70b731c4d1c16a216c4505e4fcb3be5da5
Tymko Tecca <tymko.tecca@gmail.com>
2022-05-30 14:32:11 -0400

    UBREAKIFIX

--- src/api/formatCards/extractTypeLine.ts
+++ src/api/formatCards/extractTypeLine.ts
@@ -2,7 +2,7 @@ import get from "lodash/get"
 
 const extractTypeLine = (card:RawCard):string=>{
     const main = get(card, 'type_line')
-    const dfcs = card.card_faces.map(face=>face.type_line).join(" ")
+    const dfcs = card?.card_faces?.map(face=>face.type_line).join(" ") || ""
 
     return main || dfcs || ""
 }
--- src/api/formatCards/index.ts
+++ src/api/formatCards/index.ts
@@ -5,33 +5,38 @@ import mapLegalities from "./mapLegalities";
 
 type FormatCard = (card: RawCard) => Card;
 const formatCard: FormatCard = (card) => {
-  const { id, cmc, color_identity: identity } = card;
-  const { imageUrl, reverseUrl, face } = processCardFaces(card);
-  const { colors, name, mana_cost: cost, type_line: type } = face;
-  const partnerQuery = getPartner(card);
-  const isUnlimited = getIsUnlimited(card);
-  const legalFormats = mapLegalities(card);
-
-  return {
-    attributes: {},
-    cmc,
-    colors,
-    cost,
-    count: 0,
-    id,
-    identity,
-    imageUrl,
-    isUnlimited,
-    legalFormats,
-    name,
-    partnerQuery,
-    reverseUrl,
-    sideboardCount: 0,
-    type,
-  };
+  try {
+    const { id, cmc, color_identity: identity } = card;
+    const { imageUrl, reverseUrl, face } = processCardFaces(card);
+    const { colors, name, mana_cost: cost, type_line: type } = face;
+    const partnerQuery = getPartner(card);
+    const isUnlimited = getIsUnlimited(card);
+    const legalFormats = mapLegalities(card);
+  
+    return {
+      attributes: {},
+      cmc,
+      colors,
+      cost,
+      count: 0,
+      id,
+      identity,
+      imageUrl,
+      isUnlimited,
+      legalFormats,
+      name,
+      partnerQuery,
+      reverseUrl,
+      sideboardCount: 0,
+      type,
+    };
+  }catch(e){
+    console.log(e)
+    return null
+  }
 };
 
 type FormatCards = (cards: RawCard[]) => Card[];
-const formatCards: FormatCards = (cards) => cards.map(formatCard);
+const formatCards: FormatCards = (cards) => cards.map(formatCard).filter(t=>!!t);
 
 export default formatCards;
